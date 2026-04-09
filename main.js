79e56e198b771ecaf06a564469dd711bbe7fb929
olloberdi <ohunpolatov.o@gmail.com>
2026-04-09 13:25:54 +0500

    1-masalada uch honali sonning birliklari va onliklari topildi 2-masalada uch honali sonning raqamlar yigindisi paydo boldi

---
+++ main.js
@@ -0,0 +1,43 @@
+//Integer1. Uch xonali son berilgan. Oldin uni birliklar xonasidagi raqamni so`ng o`nliklar xonasidagi raqamni chiqaruvchi programma tuzilsin.
+//let son = +prompt("uch xonali son kiriting");
+//let birlik = son % 10;
+//let onlik = Math.trunc((son / 10) % 10);
+//console.log("birliklar:  " + birlik + "  onliklar:" + onlik);
+//=============================================================================
+//Integer2. Uch xonali son berilgan. Uning raqamlar yig`indisini aniqlovchi programma tuzilsin.
+//let son = +prompt("Uch xonali son kiriting:");
+
+//let yuzlar = Math.trunc(son / 100);
+//let onliklar = Math.trunc((son / 10) % 10);
+//let birliklar = son % 10;
+
+//let yigindi = yuzlar + onliklar + birliklar;
+
+//console.log("Raqamlar yig'indisi: " + yigindi);
+//====================================================================================
+//Integer3. Uch xonali son berilgan. Uning raqamlarini teskari tartibda yozishdan hosil bo`lgan sonni aniqlovchi program tuzilsin.
+
+//Integer4. Uch xonali son berilgan. Uning o`nliklar xonasidagi raqam bilan yuzliklar xonasidagi raqamni almashtirishdan hosil bo`lgan sonni aniqlovchi programma tuzilsin.
+//Input: 123
+//Output: 213
+
+//Integer5. Uch xonali son berilgan. Uning o`nliklar xonasidagi raqam bilan birliklar xonasidagi raqamni almashtirishdan hosil bo`lgan sonni aniqlovchi programma tuzilsin.
+//Input: 123
+//Output: 132
+
+//Integer6. 999 dan katta bo`lgan son berilgan. Bir marta bo`lib butunni va bo`lib qoldiqni olish operatsiyasidan foydalanib berilgan sonni mingliklar xonasidagi sonni aniqlovchi programma tuzilsin.
+//Input: 98123
+//Output: 8
+
+//1 minut = 60 s
+//1 soat = 60 minut
+//1 soat = 3600s
+//1 sutka = 24 soat
+
+//Integer7. Kun boshidan boshlab N sekund vaqt o'tti. Kun boshidan boshlab qancha minut to'la o`tganligini aniqlovchi programma tuzilsin.
+
+//Integer8. Kun boshidan boshlab N sekund vaqt o'tti. Kun boshidan boshlab qancha to'la soat o`tganligini aniqlovchi programma tuzilsin.
+
+//Integer9. Kun boshidan boshlab N sekund vaqt o`tti. Kun boshidan boshlab qancha minut va sekund o'tganini aniqlovchi programma tuzilsin.
+
+//Integer10. Kun boshidan boshlab N sekund vaqt o`tti. Kun boshidan boshlab qancha soat va sekund o'tganini aniqlovchi programma tuzilsin.
